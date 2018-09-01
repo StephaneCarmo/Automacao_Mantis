7045191468a8dc3d8cc1e9da702773e29e6fe9cb
Stephane Carmo <sca.tete@gmail.com>
2018-09-01 09:19:33 -0300

    iniciando projeto

---
+++ Automacao-Base2-MantisBT/DriverFactory.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automacao_Base2_MantisBT
+{
+    public class DriverFactory
+    {
+        public static IWebDriver driver { get; set; }
+    }
+}
---
+++ Automacao-Base2-MantisBT/Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automacao_Base2_MantisBT
+{   
+    [TestFixture]
+    public class Test : DriverFactory
+    {   
+        [Test]
+        public void executeTest()
+        {
+
+        }
+    }
+}
